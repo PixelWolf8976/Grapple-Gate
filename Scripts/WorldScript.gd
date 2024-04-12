604f5e480b5a1a80c871546862d93df250e76cde
PixelWolf8976 <156145502+PixelWolf8976@users.noreply.github.com>
2024-04-12 14:34:42 -0400

    Player things

---
+++ Scripts/WorldScript.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+var peer := ENetMultiplayerPeer.new()
+@export var playerScene : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_host_pressed():
+	peer.create_server(1027)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	add_player()
+	$Multiplayer.hide()
+
+func _on_join_pressed():
+	peer.create_client("127.0.0.1", 1027)
+	multiplayer.multiplayer_peer = peer
+
+func add_player(id = 1):
+	var player := playerScene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+
+func exit_game(id):
+	multiplayer.peer_disconnected.connect(del_player)
+	del_player(id)
+
+func del_player(id):
+	rpc("_del_player", id)
+
+@rpc("any_peer", "call_local")
+func _del_player(id):
+	get_node(str(id)).queue_free()
