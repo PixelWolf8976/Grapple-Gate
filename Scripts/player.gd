70b9199c45d364c03a86b1f0aaf44a51cb88a490
PixelWolf8976 <156145502+PixelWolf8976@users.noreply.github.com>
2024-04-13 20:50:50 -0400

    Added better movement and grapple line

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,21 +1,24 @@
 extends CharacterBody3D
 
-@export var walkSpeed := 1000.0
-@export var jumpStrength := 250.0
+@export var walkSpeed := 500.0
+@export var jumpStrength := 300.0
 @export var frictionSubtractor := 0.75
 @export var gravity := 9.8
+@export var grappleLine := MeshInstance3D.new()
 
 @onready var twistPivot := $TwistPivot
 @onready var pitchPivot := $TwistPivot/PitchPivot
 
 @onready var mesh := $MeshInstance3D
 @onready var collision := $CollisionShape3D
+@onready var aim := $TwistPivot/PitchPivot/RayCast3D
 
 var mouseSensitivity := 0.001
 var twistInput := 0.0
 var pitchInput := 0.0
 var speed := walkSpeed
 var isCrouching := false
+var isGrappleing := false
 
 @onready var cam := $TwistPivot/PitchPivot/Camera3D
 
@@ -30,6 +33,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if is_multiplayer_authority():
+		if grappleLine != null:
+			get_tree().get_root().remove_child(grappleLine)
+			grappleLine.queue_free()
 		velocity.y += -gravity * delta
 		if is_on_floor():
 			velocity.x = 0
@@ -49,7 +55,7 @@ func _process(delta):
 		if is_on_floor() && !isCrouching && Input.is_action_pressed("Jump"):
 			velocity.y += jumpStrength * delta
 		
-		if is_on_floor():
+		if is_on_floor() && !isGrappleing:
 			var fdirection := Vector3(0, 0, -1).rotated(Vector3(0, 1, 0), mesh.rotation.y)
 			var ldircetion := Vector3(0, 0, -1).rotated(Vector3(0, 1, 0), mesh.rotation.y + deg_to_rad(90))
 			if Input.is_action_pressed("Walk Forward"):
@@ -73,6 +79,20 @@ func _process(delta):
 		twistInput = 0.0
 		pitchInput = 0.0
 		
+		if Input.is_action_pressed("Equipment Primary"):
+			isGrappleing = true
+		if Input.is_action_just_released("Equipment Primary"):
+			isGrappleing = false
+		
+		if isGrappleing:
+			if aim.is_colliding():
+				var grapHitPoint = aim.get_collision_point()
+				grappleLine = grapLine(self.position, grapHitPoint)
+				get_tree().get_root().add_child(grappleLine)
+				isGrappleing = true
+			else:
+				isGrappleing = false
+		
 		if Input.is_action_just_pressed("ui_cancel") && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		elif Input.is_action_just_pressed("ui_accept") && Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
@@ -82,6 +102,42 @@ func _process(delta):
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	move_and_slide()
+
+func grapLine(pos1: Vector3, pos2: Vector3, color = Color.SADDLE_BROWN, thickness: float = 0.1) -> MeshInstance3D:
+	var meshInstance = MeshInstance3D.new()
+	var immediateMesh = ImmediateMesh.new()
+	var material = ORMMaterial3D.new()
+	
+	meshInstance.mesh = immediateMesh
+	
+	var dir = (pos2 - pos1).normalized()
+	var right = dir.cross(Vector3(0, 1, 0)).normalized()
+	var up = right.cross(dir).normalized()
+	
+	var halfThickness = thickness / 2.0
+	
+	var p1 = pos1 - right * halfThickness
+	var p2 = pos2 - right * halfThickness
+	var p3 = pos2 + right * halfThickness
+	var p4 = pos1 + right * halfThickness
+	
+	immediateMesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES, material)
+	
+	immediateMesh.surface_add_vertex(p1)
+	immediateMesh.surface_add_vertex(p2)
+	immediateMesh.surface_add_vertex(p3)
+	
+	immediateMesh.surface_add_vertex(p3)
+	immediateMesh.surface_add_vertex(p4)
+	immediateMesh.surface_add_vertex(p1)
+	
+	immediateMesh.surface_end()
+	
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = color
+	
+	return meshInstance
+
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseMotion:
